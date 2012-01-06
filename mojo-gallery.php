4a0dc547484f41eacedd72029fd721ed2ab375db
Will Wilson <mojowill@mac.com>
2012-01-06 13:08:02 +0000

    Adding options page
    Doesn't work by the way ;)

--- mojo-gallery.php
+++ mojo-gallery.php
@@ -56,6 +56,8 @@ if ( ! class_exists( 'mojoGallery' ) ) :
 		 */
 		function __construct() {
 			
+			new mojoGalleryOptions;
+			
 			add_action( 'init', array( &$this, 'register_cpt_mojo_album' ) );
 			add_action( 'init', array( &$this, 'register_taxonomy_album_tag' ) );
 			add_action( 'init', array( &$this, 'register_taxonomy_album_category' ) );
@@ -236,6 +238,212 @@ if ( ! class_exists( 'mojoGallery' ) ) :
 		
 	} //end class
 
+endif; //end class if
+
+if ( ! class_exists( 'mojoGalleryOptions' ) ) :
+
+	/**
+	 * mojoGalleryOptions class.
+	 */
+	class mojoGalleryOptions {
+		
+		/**
+		 * __construct function.
+		 *
+		 * Stick actions and filters in here.
+		 * 
+		 * @access public
+		 * @return void
+		 */
+		function __construct() {
+			register_activation_hook( __FILE__, array( &$this, 'add_defaults' ) );
+			//register_uninstall_hook( __FILE__, array( &$this, 'delete_plugin_options' ) );
+			
+			add_action( 'admin_init', array( &$this, 'options_init' ) );
+			add_action( 'admin_menu', array( &$this, 'add_options_page' ) );
+		}
+		
+		/**
+		 * add_defaults function.
+		 * 
+		 * @access public
+		 * @return void
+		 */
+		function add_defaults() {
+			$tmp = get_option('mojoGallery_options');
+		    if ( ( $tmp['chk_default_options_db'] == '1' ) || ( ! is_array( $tmp ) ) ) :
+		    
+				delete_option('mojoGallery_options'); // so we don't have to reset all the 'off' checkboxes too! (don't think this is needed but leave for now)
+				
+				$arr = array(	"chk_button1" => "1",
+								"chk_button3" => "1",
+								"textarea_one" => esc_html( "This type of control allows a large amount of information to be entered all at once. Set the 'rows' and 'cols' attributes to set the width and height." ),
+								"txt_one" => "Enter whatever you like here..",
+								"drp_select_box" => "four",
+								"chk_default_options_db" => "",
+								"rdo_group_one" => "one",
+								"rdo_group_two" => "two"
+				);
+				
+				update_option('mojoGallery_options', $arr);
+			
+			endif;
+
+		}
+		
+		static function delete_plugin_options() {
+			delete_option( 'mojoGallery_options' );
+		}
+		
+		/**
+		 * options_init function.
+		 * 
+		 * @access public
+		 * @return void
+		 */
+		function options_init() {
+			register_setting( 'mojoGallery_plugin_options', 'mojoGallery_options', array( &$this, 'validate_options' ) );		
+		}
+		
+		/**
+		 * add_options_page function.
+		 * 
+		 * @access public
+		 * @return void
+		 */
+		function add_options_page() {
+			//add_menu_page( __('Mojo Gallery Options', 'mojo-gallery'), __('Mojo Gallery Options', 'mojo-gallery'), 'manage_options', 'mojo-options', array( &$this, 'render_form'));
+			add_submenu_page( 'edit.php?post_type=mojo-gallery-album', __('Mojo Gallery Options', 'mojo-gallery' ), __('Options', 'mojo-gallery'), 'manage_options', array( $this, 'render_form' )  );
+		}
+		
+		/**
+		 * mojoGallery_render_form function.
+		 * 
+		 * @access public
+		 * @return void
+		 */
+		function render_form() {
+		
+		?>
+			<div class="wrap">
+				
+				<!-- Display Plugin Icon, Header, and Description -->
+				<div class="icon32" id="icon-options-general"><br></div>
+				<h2><?php echo _e( 'Ultimate Wordpress CRM Plugin', 'uwpcrm' );?></h2>
+				<p><?php echo _e( 'Below are some sample controls that will be amended during plugin development.', 'uwpcrm' );?></p>
+		
+				<!-- Beginning of the Plugin Options Form -->
+				<form method="post" action="options.php">
+					<?php settings_fields('mojoGallery_plugin_options'); ?>
+					<?php $options = get_option('mojoGallery_options'); ?>
+		
+					<!-- Table Structure Containing Form Controls -->
+					<!-- Each Plugin Option Defined on a New Table Row -->
+					<table class="form-table">
+		
+						<!-- Text Area Control -->
+						<tr>
+							<th scope="row"><?php echo _e( 'Sample Text Area', 'uwpcrm' );?></th>
+							<td>
+								<textarea name="uwpcrm_options[textarea_one]" rows="7" cols="50" type='textarea'><?php echo $options['textarea_one']; ?></textarea><br /><span style="color:#666666;margin-left:2px;"><?php echo _e( 'Add a comment here to give extra information to Plugin users', 'uwpcrm' );?></span>
+							</td>
+						</tr>
+		
+						<!-- Textbox Control -->
+						<tr>
+							<th scope="row"><?php echo _e( 'Enter Some Information', 'uwpcrm' );?></th>
+							<td>
+								<input type="text" size="57" name="uwpcrm_options[txt_one]" value="<?php echo $options['txt_one']; ?>" />
+							</td>
+						</tr>
+		
+						<!-- Radio Button Group -->
+						<tr valign="top">
+							<th scope="row"><?php echo _e( 'Radio Button Group #1', 'uwpcrm' );?></th>
+							<td>
+								<!-- First radio button -->
+								<label><input name="uwpcrm_options[rdo_group_one]" type="radio" value="one" <?php checked('one', $options['rdo_group_one']); ?> /> <?php echo _e( 'Radio Button #1',' uwpcrm' );?></label><br />
+		
+								<!-- Second radio button -->
+								<label><input name="uwpcrm_options[rdo_group_one]" type="radio" value="two" <?php checked('two', $options['rdo_group_one']); ?> /> <?php echo _e( 'Radio Button #2', 'uwpcrm' );?></label><br /><span style="color:#666666;"><?php echo _e( 'General comment to explain more about this Plugin option.', 'uwpcrm' );?></span>
+							</td>
+						</tr>
+		
+						<!-- Checkbox Buttons -->
+						<tr valign="top">
+							<th scope="row"><?php echo _e( 'Group of Checkboxes', 'uwpcrm' );?></th>
+							<td>
+								<!-- First checkbox button -->
+								<label><input name="uwpcrm_options[chk_button1]" type="checkbox" value="1" <?php if (isset($options['chk_button1'])) { checked('1', $options['chk_button1']); } ?> /> <?php echo _e( 'Checkbox #1', 'uwpcrm' );?></label><br />
+		
+								<!-- Second checkbox button -->
+								<label><input name="uwpcrm_options[chk_button2]" type="checkbox" value="1" <?php if (isset($options['chk_button2'])) { checked('1', $options['chk_button2']); } ?> /> <?php echo _e( 'Checkbox #2', 'uwpcrm' );?></label><br />
+		
+								<!-- Third checkbox button -->
+								<label><input name="uwpcrm_options[chk_button3]" type="checkbox" value="1" <?php if (isset($options['chk_button3'])) { checked('1', $options['chk_button3']); } ?> /> <?php echo _e( 'Checkbox #3', 'uwpcrm' );?></label><br />
+		
+								<!-- Fourth checkbox button -->
+								<label><input name="uwpcrm_options[chk_button4]" type="checkbox" value="1" <?php if (isset($options['chk_button4'])) { checked('1', $options['chk_button4']); } ?> /> <?php echo _e( 'Checkbox #4', 'uwpcrm' );?> </label><br />
+		
+								<!-- Fifth checkbox button -->
+								<label><input name="uwpcrm_options[chk_button5]" type="checkbox" value="1" <?php if (isset($options['chk_button5'])) { checked('1', $options['chk_button5']); } ?> /> <?php echo _e( 'Checkbox #5', 'uwpcrm' );?> </label>
+							</td>
+						</tr>
+		
+						<!-- Another Radio Button Group -->
+						<tr valign="top">
+							<th scope="row">Radio Button Group #2</th>
+							<td>
+								<!-- First radio button -->
+								<label><input name="uwpcrm_options[rdo_group_two]" type="radio" value="one" <?php checked('one', $options['rdo_group_two']); ?> /> Radio Button #1</label><br />
+		
+								<!-- Second radio button -->
+								<label><input name="uwpcrm_options[rdo_group_two]" type="radio" value="two" <?php checked('two', $options['rdo_group_two']); ?> /> Radio Button #2</label><br />
+		
+								<!-- Third radio button -->
+								<label><input name="uwpcrm_options[rdo_group_two]" type="radio" value="three" <?php checked('three', $options['rdo_group_two']); ?> /> Radio Button #3</label>
+							</td>
+						</tr>
+		
+						<!-- Select Drop-Down Control -->
+						<tr>
+							<th scope="row">Sample Select Box</th>
+							<td>
+								<select name='uwpcrm_options[drp_select_box]'>
+									<option value='one' <?php selected('one', $options['drp_select_box']); ?>>One</option>
+									<option value='two' <?php selected('two', $options['drp_select_box']); ?>>Two</option>
+									<option value='three' <?php selected('three', $options['drp_select_box']); ?>>Three</option>
+									<option value='four' <?php selected('four', $options['drp_select_box']); ?>>Four</option>
+									<option value='five' <?php selected('five', $options['drp_select_box']); ?>>Five</option>
+									<option value='six' <?php selected('six', $options['drp_select_box']); ?>>Six</option>
+									<option value='seven' <?php selected('seven', $options['drp_select_box']); ?>>Seven</option>
+									<option value='eight' <?php selected('eight', $options['drp_select_box']); ?>>Eight</option>
+								</select>
+								<span style="color:#666666;margin-left:2px;">Add a comment here to explain more about how to use the option above</span>
+							</td>
+						</tr>
+		
+						<tr><td colspan="2"><div style="margin-top:10px;"></div></td></tr>
+						<tr valign="top" style="border-top:#dddddd 1px solid;">
+							<th scope="row">Database Options</th>
+							<td>
+								<label><input name="uwpcrm_options[chk_default_options_db]" type="checkbox" value="1" <?php if (isset($options['chk_default_options_db'])) { checked('1', $options['chk_default_options_db']); } ?> /> Restore defaults upon plugin deactivation/reactivation</label>
+								<br /><span style="color:#666666;margin-left:2px;">Only check this if you want to reset plugin settings upon Plugin reactivation</span>
+							</td>
+						</tr>
+					</table>
+					<p class="submit">
+					<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+					</p>
+				</form>
+			</div>
+			<?php	
+
+		}
+		
+
+				
+	} //end class
 endif; //end class if
 
 /**
