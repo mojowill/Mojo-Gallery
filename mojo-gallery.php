d3cfb4628cd124db2fa0d4d446c39754dc9c5dcf
mojowill <mojowill@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-09 20:49:24 +0000

    Adding custom taxonomies and some output filters
    git-svn-id: http://plugins.svn.wordpress.org/mojo-gallery/trunk@487159 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mojo-gallery.php
+++ mojo-gallery.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: The Mojo Gallery
-Plugin URI: http://www.mojowill.com/
-Description: A small gallery plugin using the built in media uploader and gallery shortcodes
-Version: 1.0
+Plugin URI: http://www.mojowill.com/developer/mojo-gallery-plugin/
+Description: A small gallery plugin using the built in media uploader and gallery shortcodes. THIS IS A WORK IN PROGRESS!
+Version: 0.1
 Author: theMojoWill
 Author URI: http://www.mojowill.com
 License: GPLv2 or later
@@ -71,6 +71,10 @@ if ( ! class_exists( 'mojoGallery' ) ) :
 		function __construct() {
 			
 			add_action( 'init', array( &$this, 'register_cpt_mojo_album' ) );
+			add_action( 'init', array( &$this, 'register_taxonomy_album_tag' ) );
+			add_action( 'init', array( &$this, 'register_taxonomy_album_category' ) );
+			
+			add_filter( 'the_content', array( &$this, 'output_gallery' ) );
 
 		}
 		
@@ -85,26 +89,26 @@ if ( ! class_exists( 'mojoGallery' ) ) :
 		function register_cpt_mojo_album() {
 
 		    $labels = array( 
-		        'name' => _x( 'Albums', 'mojo-gallery' ),
-		        'singular_name' => _x( 'Album', 'mojo-gallery' ),
-		        'add_new' => _x( 'Add New', 'mojo-gallery' ),
-		        'add_new_item' => _x( 'Add New Album', 'mojo-gallery' ),
-		        'edit_item' => _x( 'Edit Album', 'mojo-gallery' ),
-		        'new_item' => _x( 'New Album', 'mojo-gallery' ),
-		        'view_item' => _x( 'View Album', 'mojo-gallery' ),
-		        'search_items' => _x( 'Search Albums', 'mojo-gallery' ),
-		        'not_found' => _x( 'No albums found', 'mojo-gallery' ),
-		        'not_found_in_trash' => _x( 'No albums found in Trash', 'mojo-gallery' ),
-		        'parent_item_colon' => _x( 'Parent Album:', 'mojo-gallery' ),
-		        'menu_name' => _x( 'Mojo Gallery', 'mojo-gallery' ),
+		        'name' => __( 'Albums', 'mojo-gallery' ),
+		        'singular_name' => __( 'Album', 'mojo-gallery' ),
+		        'add_new' => __( 'Add Album', 'mojo-gallery' ),
+		        'add_new_item' => __( 'Add New Album', 'mojo-gallery' ),
+		        'edit_item' => __( 'Edit Album', 'mojo-gallery' ),
+		        'new_item' => __( 'New Album', 'mojo-gallery' ),
+		        'view_item' => __( 'View Album', 'mojo-gallery' ),
+		        'search_items' => __( 'Search Albums', 'mojo-gallery' ),
+		        'not_found' => __( 'No albums found', 'mojo-gallery' ),
+		        'not_found_in_trash' => __( 'No albums found in Trash', 'mojo-gallery' ),
+		        'parent_item_colon' => __( 'Parent Album:', 'mojo-gallery' ),
+		        'menu_name' => __( 'Mojo Gallery', 'mojo-gallery' ),
 		    );
 		
 		    $args = array( 
 		        'labels' => $labels,
 		        'hierarchical' => true,
 		        
-		        'supports' => array( 'title', 'editor', 'thumbnail', 'post-formats' ),
-		        'taxonomies' => array( 'custom-tax' ),
+		        'supports' => array( 'title', 'editor', 'thumbnail', 'page-attributes' ),
+		        'taxonomies' => array( 'album-tag', 'album_category' ),
 		        'public' => true,
 		        'show_ui' => true,
 		        'show_in_menu' => true,
@@ -121,7 +125,118 @@ if ( ! class_exists( 'mojoGallery' ) ) :
 		    );
 		
 		    register_post_type( 'mojo-gallery-album', $args );
-		}		
+		}
+		
+		/**
+		 * register_taxonomy_album_tag function.
+		 *
+		 * Setup our album tags
+		 * 
+		 * @access public
+		 * @return void
+		 */
+		function register_taxonomy_album_tag() {
+
+		    $labels = array( 
+		        'name' => __( 'Album Tags', 'album tag' ),
+		        'singular_name' => __( 'Album Tag', 'album tag' ),
+		        'search_items' => __( 'Search Album Tags', 'album tag' ),
+		        'popular_items' => __( 'Popular Album Tags', 'album tag' ),
+		        'all_items' => __( 'All Album Tags', 'album tag' ),
+		        'parent_item' => __( 'Parent Album Tag', 'album tag' ),
+		        'parent_item_colon' => __( 'Parent Album Tag:', 'album tag' ),
+		        'edit_item' => __( 'Edit Album Tag', 'album tag' ),
+		        'update_item' => __( 'Update Album Tag', 'album tag' ),
+		        'add_new_item' => __( 'Add New Album Tag', 'album tag' ),
+		        'new_item_name' => __( 'New Album Tag Name', 'album tag' ),
+		        'separate_items_with_commas' => __( 'Separate album tags with commas', 'album tag' ),
+		        'add_or_remove_items' => __( 'Add or remove album tags', 'album tag' ),
+		        'choose_from_most_used' => __( 'Choose from the most used album tags', 'album tag' ),
+		        'menu_name' => __( 'Album Tags', 'album tag' ),
+		    );
+		
+		    $args = array( 
+		        'labels' => $labels,
+		        'public' => true,
+		        'show_in_nav_menus' => false,
+		        'show_ui' => true,
+		        'show_tagcloud' => true,
+		        'hierarchical' => false,
+		
+		        'rewrite' => true,
+		        'query_var' => true
+		    );
+		
+		    register_taxonomy( 'album_tag', array('mojo-gallery-album'), $args );
+
+		}
+		
+		/**
+		 * register_taxonomy_album_category function.
+		 *
+		 * Setup our categories
+		 * 
+		 * @access public
+		 * @return void
+		 */
+		function register_taxonomy_album_category() {
+
+		    $labels = array( 
+		        'name' => __( 'Album Categories', 'album category' ),
+		        'singular_name' => __( 'Album Category', 'album category' ),
+		        'search_items' => __( 'Search Album Categories', 'album category' ),
+		        'popular_items' => __( 'Popular Album Categories', 'album category' ),
+		        'all_items' => __( 'All Album Categories', 'album category' ),
+		        'parent_item' => __( 'Parent Album Category', 'album category' ),
+		        'parent_item_colon' => __( 'Parent Album Category:', 'album category' ),
+		        'edit_item' => __( 'Edit Album Category', 'album category' ),
+		        'update_item' => __( 'Update Album Category', 'album category' ),
+		        'add_new_item' => __( 'Add New Album Category', 'album category' ),
+		        'new_item_name' => __( 'New Album Category Name', 'album category' ),
+		        'separate_items_with_commas' => __( 'Separate album categories with commas', 'album category' ),
+		        'add_or_remove_items' => __( 'Add or remove album categories', 'album category' ),
+		        'choose_from_most_used' => __( 'Choose from the most used album categories', 'album category' ),
+		        'menu_name' => __( 'Album Categories', 'album category' ),
+		    );
+		
+		    $args = array( 
+		        'labels' => $labels,
+		        'public' => true,
+		        'show_in_nav_menus' => true,
+		        'show_ui' => true,
+		        'show_tagcloud' => false,
+		        'hierarchical' => true,
+		
+		        'rewrite' => true,
+		        'query_var' => true
+		    );
+		
+		    register_taxonomy( 'album_category', array('mojo-gallery-album'), $args );
+
+		}
+		
+		/**
+		 * output_gallery function.
+		 *
+		 * Adds the Gallery Shortcode to the_content on album pages 
+		 * ONLY if they haven't already added the shortcode!
+		 * 
+		 * @access public
+		 * @return void
+		 */
+		function output_gallery() {
+			global $post;
+			
+			if ( 'mojo-gallery-album' == get_post_type() && is_singular()  ) :
+			
+				if ( true !== strpos( $post->post_content, '[gallery' ) ) :
+				
+					echo do_shortcode('[gallery]');
+				
+				endif;
+			
+			endif;
+		}
 		
 	} //end class
 
