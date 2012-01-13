9b1aa5b688620cf81f1974ef4c9dc13c65a795be
mojowill <mojowill@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-13 15:50:56 +0000

    Still working on front end implementation
    git-svn-id: http://plugins.svn.wordpress.org/mojo-gallery/trunk@489436 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/single-mojo_gallery_album.php
+++ templates/single-mojo_gallery_album.php
@@ -3,63 +3,56 @@
 			<div id="content3">
 				
 				<?php if (have_posts()) : ?>
-				<div class="fullcol">
             		<?php while (have_posts()) : the_post();
             			
             			//Lets get some info about our parent post
             			$parent_title = get_the_title($post->post_parent);
             			
-            			//Get options
-            			$options = get_option( 'mojoGallery_options' ); 
             			//If we have a parent post modify the link/title to include it
             			if ($post->post_parent ) : ?>
             			
-            				<h1><a href="<?php echo get_post_type_archive_link( 'mojo-gallery-album' );?>"><?php echo _e( 'Gallery', 'mojo-gallery' );?></a> &raquo; <a href="<?php echo get_permalink($post->post_parent);?>"><?php echo $parent_title;?></a> &raquo; <?php the_title();?></h1>			
+            				<h1><a href="<?php echo get_post_type_archive_link( 'mojo-gallery-album' );?>"><?php $mojoGallery->archive_title();?></a> &raquo; <a href="<?php echo get_permalink($post->post_parent);?>"><?php echo $parent_title;?></a> &raquo; <?php the_title();?></h1>			
 						
 						<?php else : ?>
 					
-							<h1><a href="<?php echo get_post_type_archive_link( 'mojo-gallery-album');?>"><?php echo _e( 'Gallery', 'mojo-gallery');?></a> &raquo; <?php the_title();?></h1>						
+							<h1><a href="<?php echo get_post_type_archive_link( 'mojo-gallery-album');?>"><?php $mojoGallery->archive_title();?></a> &raquo; <?php the_title();?></h1>						
 						<?php endif;
 					
 						//If we have no content and we have children list them, we need to check here for out own injection of the shortcode!
-						if ( ( get_the_content() == '' ) && ( ! $mojoGallery->has_attachements() ) ) :
+						if ( ( get_the_content() == '' ) || ( ! $mojoGallery->has_attachments() ) ) :
 							
 							$args = array(
-								'post_type' => 'gallery',
+								'post_type' => 'mojo-gallery-album',
 								'child_of' => $post->ID,
 								'parent' => $post->ID,
 							);
 							
 							$albums = get_pages($args);
-							
 							$counter = 1; //start our counter
-							$grids = $options['columns']; //images per row should be the same as archive-gallery.php
 							?>
 							
-							<div id="gridcontainer">
+							<div id="mojoGallery">
 							
 							<?php foreach ( $albums as $album ) {
-
-								$permalink = get_permalink( $album->ID );
 																
 								//show left hand column
-								if ( $counter != $grids ) : ?>
+								if ( $counter != $mojoGallery->column_count() ) : ?>
 									<div class="griditemleft">
 										<div class="postimage">
-											<a href="<?php echo $permalink;?>"><?php echo $mojoGallery->default_thumbnails();?></a>
+											<a href="<?php $mojoGallery->album_permalink();?>"><?php $mojoGallery->default_thumbnails();?></a>
 										</div>
-										<h2><a href="<?php echo $permalink;?>"><?php echo $album->post_title;?></a></h2>
+										<h2><a href="<?php $mojoGallery->album_permalink();?>"><?php echo $album->post_title;?></a></h2>
 									</div>
 									
 								<?php
 								//show the right hand column
-								elseif ( $counter == $grids ) : ?>
+								elseif ( $counter == $mojoGallery->column_count() ) : ?>
 								
 									<div class="griditemright">
 										<div class="postimage">
-											<a href="<?php echo $permalink;?>"><?php echo $mojoGallery->default_thumbnails();?></a>
+											<a href="<?php $mojoGallery->album_permalink();?>"><?php $mojoGallery->default_thumbnails();?></a>
 										</div>
-										<h2><a href="<?php echo $permalink;?>"><?php echo $album->post_title;?></a></h2>								
+										<h2><a href="<?php $mojoGallery->album_permalink();?>"><?php echo $album->post_title;?></a></h2>								
 									</div>
 									
 									<div class="clear"></div>
@@ -84,8 +77,10 @@
 					
 					
 					endwhile; ?>
-				</div>	                   
-                
+				<?php else : ?>
+				
+					<p><?php echo _e( 'Sorry, no album found.', 'mojo-gallery' );?></p>
+					
                 <?php endif; ?>
                 			
 			</div>
